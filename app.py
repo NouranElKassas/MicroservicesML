38ac614865f07f83632a1e7f5a08bf592afc02e4
Nouran El Kassas <nouran.elkassas96@gmail.com>
2020-06-03 23:03:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def scale(payload):
 @app.route("/")
 def home():
     
-    return html.format("<h3>Sklearn Prediction Home</h3>")
+    return html.format('<h3>Sklearn Prediction Home</h3>')
 
 @app.route("/predict", methods=['POST'])
 def predict():
