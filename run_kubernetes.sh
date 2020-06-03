ae554c338672708094f7e7fb8723e07f29b68d08
Nouran El Kassas <nouran.elkassas96@gmail.com>
2020-06-03 20:30:57 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=udacitypinzonimage
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run $dockerpath --image=andresaaap/udacitypinzonimage:v1pro3 --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment udacitypinzonimage --type=LoadBalancer --port=80
+minikube service udacitypinzonimage
